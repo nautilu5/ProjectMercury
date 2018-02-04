a1f8b0fa298963c3fc4306e4dfd22349316da729
nautilu5 <rockwe46@msu.edu>
2018-02-03 20:44:21 -0500

    Added mongoose template script
    The following script demonstrates how to connect to a mongodb server and add an item via mongoose.

---
+++ mongoose.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
+
+mongoose.connect('mongodb://localhost:27017/local');
+
+var SchemaTypes = mongoose.Schema.Types;
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+    var itemSchema = mongoose.Schema({
+        name: String,
+        price: SchemaTypes.Double,
+        numreviews: SchemaTypes.Double,
+        avgreviews: SchemaTypes.Double,
+        address: {
+            street: String,
+            zipcode: Number,
+            building: Number
+        },
+        manufacturer: String
+    });
+
+    itemSchema.methods.speak = function () {
+        var greeting = this.name
+        ? "The item name is " + this.name
+        : "No item name";
+        console.log(greeting);
+    }
+
+    var item = mongoose.model('Items', itemSchema);
+
+    var testitem = new item({ 
+        name: 'String',
+        price: 12.01,
+        numreviews: 12.01,
+        avgreviews: 12.01,
+        address: {
+            street: '101 Test Drive',
+            zipcode: 49001,
+            building: 100
+        },
+        manufacturer: 'Test'
+    });
+    
+    testitem.save(function (err, testitem) {
+        if (err) return console.error(err);
+        testitem.speak();
+    });
+});
